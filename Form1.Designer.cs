0b5c9d454c5d18f6af5d82dfd8e7613735c8c546
Stevam <129697376+stevammm@users.noreply.github.com>
2024-01-11 15:32:00 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_usuario = new System.Windows.Forms.TextBox();
+            this.tb_senha = new System.Windows.Forms.TextBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tb_status = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_usuario
+            // 
+            this.tb_usuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.tb_usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_usuario.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_usuario.ForeColor = System.Drawing.Color.White;
+            this.tb_usuario.Location = new System.Drawing.Point(199, 165);
+            this.tb_usuario.Name = "tb_usuario";
+            this.tb_usuario.Size = new System.Drawing.Size(163, 14);
+            this.tb_usuario.TabIndex = 6;
+            this.tb_usuario.Text = "Usuário:";
+            this.tb_usuario.Click += new System.EventHandler(this.textBox1_TextChanged);
+            this.tb_usuario.TextChanged += new System.EventHandler(this.tb_usuario_TextChanged);
+            // 
+            // tb_senha
+            // 
+            this.tb_senha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.tb_senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_senha.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_senha.ForeColor = System.Drawing.Color.White;
+            this.tb_senha.Location = new System.Drawing.Point(199, 218);
+            this.tb_senha.Name = "tb_senha";
+            this.tb_senha.PasswordChar = '*';
+            this.tb_senha.Size = new System.Drawing.Size(163, 14);
+            this.tb_senha.TabIndex = 7;
+            this.tb_senha.Text = "Senha:";
+            this.tb_senha.UseSystemPasswordChar = true;
+            this.tb_senha.Click += new System.EventHandler(this.textBox2_TextChanged);
+            this.tb_senha.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_senha_KeyPress);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::Login.Properties.Resources.fechar;
+            this.pictureBox6.Location = new System.Drawing.Point(450, 1);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(33, 35);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.DimGray;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(151, 294);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(211, 23);
+            this.iconButton1.TabIndex = 5;
+            this.iconButton1.Text = "Login";
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pictureBox5.Image = global::Login.Properties.Resources.whats;
+            this.pictureBox5.Location = new System.Drawing.Point(295, 323);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(35, 36);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pictureBox4.Image = global::Login.Properties.Resources.instagram;
+            this.pictureBox4.Location = new System.Drawing.Point(244, 323);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pictureBox3.Image = global::Login.Properties.Resources.face;
+            this.pictureBox3.Location = new System.Drawing.Point(190, 323);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(39, 36);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pictureBox2.Image = global::Login.Properties.Resources.senha;
+            this.pictureBox2.Location = new System.Drawing.Point(151, 203);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 35);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pictureBox1.Image = global::Login.Properties.Resources.usuario;
+            this.pictureBox1.Location = new System.Drawing.Point(151, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 40);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.panel1.Controls.Add(this.tb_status);
+            this.panel1.Location = new System.Drawing.Point(120, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 240);
+            this.panel1.TabIndex = 9;
+            // 
+            // tb_status
+            // 
+            this.tb_status.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.tb_status.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_status.ForeColor = System.Drawing.Color.White;
+            this.tb_status.Location = new System.Drawing.Point(81, 133);
+            this.tb_status.Name = "tb_status";
+            this.tb_status.Size = new System.Drawing.Size(161, 13);
+            this.tb_status.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::Login.Properties.Resources.fundo1;
+            this.ClientSize = new System.Drawing.Size(500, 530);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.tb_senha);
+            this.Controls.Add(this.tb_usuario);
+            this.Controls.Add(this.iconButton1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.TextBox tb_usuario;
+        private System.Windows.Forms.TextBox tb_senha;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb_status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            tb_usuario.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            tb_senha.Clear();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tb_usuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process IniciarLink = new System.Diagnostics.Process();
+            IniciarLink.StartInfo = 
+                new System.Diagnostics.ProcessStartInfo(@"http://www.facebook.com");
+            IniciarLink.Start();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process IniciarLink = new System.Diagnostics.Process();
+            IniciarLink.StartInfo =
+                new System.Diagnostics.ProcessStartInfo(@"http://www.instagram.com");
+            IniciarLink.Start();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process IniciarLink = new System.Diagnostics.Process();
+            IniciarLink.StartInfo =
+                new System.Diagnostics.ProcessStartInfo(@"https://www.whatsapp.com/?lang=pt_BR");
+            IniciarLink.Start();
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            String user, senha, userDigitado, senhaDigitada;
+            user = "alpha";
+            senha = "123";
+
+            userDigitado = tb_usuario.Text;
+            senhaDigitada = tb_senha.Text;
+
+            if(user.Equals(userDigitado)& senha.Equals(senhaDigitada))
+            {
+                this.Visible = false;
+                TelaPrincipal telaInicial = new TelaPrincipal();
+                telaInicial.Show();
+                
+            }
+            else
+            {
+                tb_status.Text = "Login ou senha incorreta";
+            }
+          
+        }
+
+        private void tb_senha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                String user, senha, userDigitado, senhaDigitada;
+                user = "alpha";
+                senha = "123";
+
+                userDigitado = tb_usuario.Text;
+                senhaDigitada = tb_senha.Text;
+
+                if (user.Equals(userDigitado) & senha.Equals(senhaDigitada))
+                {
+                    this.Visible = false;
+                    TelaPrincipal telaInicial = new TelaPrincipal();
+                    telaInicial.Show();
+
+                }
+                else
+                {
+                    tb_status.Text = "Login ou senha incorreta";
+                }
+            }
+        }
+    }
+}
